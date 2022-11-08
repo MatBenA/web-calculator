17517b6ce5d00ee9ff8ec039c7854d4fead86463
Matías <crosserain@gmail.com>
2022-11-08 17:55:01 -0300

    comments

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+//add two numbers return the result
 function add(aNum, bNum) {
   return aNum + bNum;
 }
 
+//subtract two numbers return the result
 function subtract(aNum, bNum) {
   return aNum - bNum;
 }
 
+//multiply two numbers return the result
 function multiply(aNum, bNum) {
   return aNum * bNum;
 }
 
+//divide two numbers return the result
 function divide(aNum, bNum) {
+    //if divisor is equal to 0 return infinity
   return bNum !== 0 ? aNum / bNum : Infinity;
 }
 
+/* 
+receives two numbers and one of the four basic arithmetical operator 
+and executes the correspondent function, returns the result
+ */
 function operate(aNum, bNum, operator) {
   switch (operator) {
     case "+":
