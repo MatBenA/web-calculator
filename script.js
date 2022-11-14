2b0174d27f01bce42b1fe0284c6c3a3ea2b67f71
Matías <crosserain@gmail.com>
2022-11-14 12:32:12 -0300

    comments modified. now the opIndex and the opSelected are rested as well whenever you clear the display

--- script.js
+++ script.js
@@ -117,9 +117,8 @@ let opSelected;
   add feature to add a minus simbol at the begining of an expression.
 */
 function main(button, type) {
-
   //clears the display when there is an infity result
-  if (resultDisp.textContent === "Infinity"){
+  if (resultDisp.textContent === "Infinity") {
     clearExp();
   }
 
@@ -132,7 +131,6 @@ function main(button, type) {
   }
 
   if (isOperator(button)) {
-    //arreglar signo menos al principio
     //checks if the button pressed is an operator
     if (expression.length > 0) {
       //checks if its not the the first place
@@ -147,7 +145,6 @@ function main(button, type) {
   }
 
   if (button === ".") {
-
     expression.push(button);
     resultDisp.textContent = expression.join("");
   }
@@ -206,6 +203,8 @@ function isOperator(button) {
 function clearExp() {
   expression.splice(0, expression.length);
   resultDisp.textContent = expression.join("");
+  opIndex = undefined;
+  opSelected = undefined;
 }
 
 function displayResult() {
@@ -215,17 +214,17 @@ function displayResult() {
   let secondTerm = expression.slice(opIndex + 1, expression.length).join(""); //selects the second term of the array
   secondTerm = parseFloat(secondTerm); //parses the term into a integer
 
+  //calcules the expression
+  let calculationResult = operate(firstTerm, opSelected, secondTerm);
+
+  //fixed to 2 decimals
+  calculationResult = fixDecimal(calculationResult);
+
   //clears the array and the display
   clearExp();
 
   //get the result spread it and store it in the expression array
-
-  let calculationResult = operate(firstTerm, opSelected, secondTerm);
-
-  calculationResult = fixDecimal(calculationResult);
-
   expression = [...`${calculationResult}`];
-
   resultDisp.textContent = expression.join("");
 }
 
@@ -252,99 +251,3 @@ function fixDecimal(calculationResult) {
     return calculationResult.toFixed(2);
   }
 }
-
-/*
-Calculator general behavior:
-  When you make your first calculation it will do it normally
-  But when you make a second calculation after the first one
-  it will use the result of the previous calculation as the
-  first number
-
-Calculator Behavior during first operation
-the first input
-When the the user first touches a button ONLY a number or minus
-sign will have effect and it will appear in the display.
-
-if its not a number or a negative sign it will not have any effect.
-
-after the first input has been made any button can be pressed.
-
-  if the user presses a number it will appear in the display
-                      next to the previous character.
-                      
-                      an operator it will appear in the display
-                      next to the previous character if it is a
-                      number, but if the previous char is another
-                      simbol it will replace it.
-                      
-                      the delete button, the last element that
-                      was entered will be deleted.
-
-                      the clear button will eliminate all the
-                      elements in the display and basically
-                      reset the calculator.
-
-                      decimal point, if the previous char is a
-                      number it will a decimal point but before
-                      it will make sure that there are no more
-                      decimal points in that same number.
-
-                      the equal button: the behavior of this
-                      button is more complex, and it will be
-                      described in the next paragraph.
-
-Equal Button Behavior:
-  *Pressing this button will only have
-   effect if the last element in the input is a number (if your
-   first input is a negative sign and right after that you press
-   the equal button it will have no effect).
-
-  *If there is only one number in the input (either positive or
-   negative) the result display will show that exact same number
-
-  *If in the display there are two numbers, separated by an operator
-   the three of these numbers will be passed as arguments to the
-   operate function.
-    *Operate function: receives two numbers and one of the four 
-     basic arithmetical operators (+ - * /) and executes the
-     correspondent function, returns the result.
-   After that the return of this function will be displayed in
-   the result display
-
-  *During next calculation: It will do the same as if it was
-   in the first calculation but will use the result of the first
-   calculation as first term.
-
-  Display behavior:
-    the whole display is made up of two smaller displays
-    the result one and the expression one.
-    
-    *Common characteristics: both displays have a limit of chars
-     that they can display, if that limit is reached it will add
-     no more characters.
-
-    *Result display: Here will appear all the new numbers and
-     symbols entered by the user and when the full expression is
-     written and the equal button or another symbol is pressed
-     this display will reset and show the result. The expression
-     that was written here is passed to the expression display.
-
-    *Expression display: will only show expressions, this will
-     happen when the result display shows the result of said
-     expression.
-    
-*/
-
-/*
-Asign an event listener to each button, this will execute
-a function that will return the number or symbol asigned to each button.
-
-when a button returns its value, we will push this value into an array
-with .push()
-
-the value of each element in this array will be displayed in the calculator
-display EXCEPT for the equal symbol.
-
-if in the display there is a number, an operator and another number
-the value of each element in the display array will go to the
-*/
